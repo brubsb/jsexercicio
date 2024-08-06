c326d0d44fab2f87ff98ef88cc5813880fbff152
Yuri Nogueira <ynogueira317@gmail.com>
2024-08-06 14:09:58 -0300

    exercicio javascript

---
+++ calcu_idade.js
@@ -0,0 +1,5 @@
+prompt = require("prompt-sync")();
+let nascimento = prompt("Digite seu ano de nacimento: ");
+let ano = new Date().getFullYear();
+let resul = parseFloat(ano) - parseFloat(nascimento);
+console.log("Sua idade é: " + resul);
---
+++ caldescont.js
@@ -0,0 +1,1 @@
+prompt = require("prompt-sync")();
